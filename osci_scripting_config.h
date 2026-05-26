a8367ae2fe397b0da4c780ec10b13fe18b9f4179
James H Ball <james@ball.sh>
2026-05-26 23:54:11 +0100

    Nest LuaJIT in scripting module

--- osci_scripting_config.h
+++ osci_scripting_config.h
@@ -1,5 +1,5 @@
 #pragma once
 
 #if !__has_include(<lua.hpp>)
-#error "osci_scripting requires LuaJIT headers. Add the LuaJIT include path or omit the osci_scripting module."
+#error "osci_scripting requires LuaJIT headers. Initialise third_party/LuaJIT and add its src directory to the include path, or omit the osci_scripting module."
 #endif
